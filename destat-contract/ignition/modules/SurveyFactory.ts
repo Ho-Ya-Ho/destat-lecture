f1d872cbf27454943ef446de83f547af25d5a293
Ho-Ya-Ho <rkdgh1056@gmail.com>
2025-11-13 21:39:07 +0900

    11 week

--- destat-contract/ignition/modules/SurveyFactory.ts
+++ destat-contract/ignition/modules/SurveyFactory.ts
@@ -1,9 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import { ethers } from "ethers";
 
-export default buildModule("CounterModule", (m) => {
-  const counter = m.contract("Counter");
-
-  m.call(counter, "incBy", [5n]);
-
-  return { counter };
+export default buildModule("SurveyFactoryModule", (m) => {
+  const surveyFactory = m.contract("SurveyFactory", [
+      ethers.parseEther("50"),
+      ethers.parseEther("0.5"),
+  ]);
+  return { surveyFactory };
 });
